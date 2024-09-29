4ec4f1ebc2cec3a83ef58bbfc81a19b888fceee4
Nacho Diez <diezgarcianacho@gmail.com>
2024-09-29 19:54:12 +0200

    casi_0

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("¿Quires saber si tú numero es casi 0?, escribbelo aquí y verás si le das al intro ;)");
+
+        Scanner sc = new Scanner(System.in);
+        float numero = sc.nextFloat();
+
+        if(numero >= 1){
+            System.out.println("No es un numero casi 0");
+        }else{
+            if(numero <= -1){
+            System.out.println("No es un numero casi 0");
+            }else{
+                if(numero == 0){
+                    System.out.println("Es 0");
+                }else{
+                    System.out.println("Es un numero casi 0");
+                }
+            }
+        }
+    }
+}
